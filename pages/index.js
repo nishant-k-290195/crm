3fadc96cdefbe6b6b5b74fa012834ffb96ca1456
Nishant Kumar <nishant290195.nkr@gmail.com>
2021-08-10 21:48:58 -0400

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to next.js</h1>
+    <div style={{padding:"4rem"}}>
+      <h1>Welcome to the CRM tool for the Website retaporta.com</h1>
+      <h3>Currently this project is still under construction.</h3>
+      <p >However you can use the "Quote" tab, "Sale Order" tab and the "Job Orders" tab from the left sidebar to create a new quote, sales order and and job orders</p>
+      <i style={{background: 'yellow'}}>I request you to ignore the ugly UI of the dashboard for a while and I can assure you will love the ease of automation to send quotes, sales orders pdf, job order pdf to your customers and vendors :)</i>
     </div>
   )
 }
